965a4e3d544009e0f2e02c359ceef0858437e8a2
Adrian Urgiles <arurgiles2@utpl.edu.ec>
2024-11-27 11:49:33 -0500

    Problemas

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
@@ -7,6 +7,10 @@
  sean ingresados por teclado por el usuario.
  */
 package ejemplos02;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -14,6 +18,8 @@ package ejemplos02;
 public class Ejemplo05 {
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
         // 2. numero, i[0, n]
         int numero;
         // 3. potencia, i[0, n]
@@ -29,7 +35,12 @@ public class Ejemplo05 {
 
         int contador = 1;
 
+        System.out.println("Ingrese el numero al que va a elevar");
+         numero = entrada.nextInt(); 
+         System.out.println("Ingrese la potencia");
+         potencia = entrada.nextInt(); 
         do {
+     
             resultado = resultado * numero;
             contador = contador + 1;
         } while (contador <= potencia);
